9237d21aa8477ecc82e825574dccb8b4219131b5
steve green <steve02081504@EDEN.fukwold>
2021-03-30 15:32:52 +0800

    Update

--- base.hpp
+++ base.hpp
@@ -4,115 +4,115 @@
 #include<string>
 #pragma comment(lib,"ws2_32.lib")
 
-
-inline struct WSA_t{
-	static inline bool inited = 0;
-	void init(){
-		if(inited)
-			return;
-		WSADATA wsaData = {0};
-		if (WSAStartup(MAKEWORD(2, 1), &wsaData) != 0)
-			throw (std::runtime_error)std::to_string(WSAGetLastError());
-	}
-	void uninit(){
-		if(inited)
-			WSACleanup();
-		inited = 0;
-	}
-	~WSA_t(){uninit();}
-}WSA{};
-
-struct Socket_link_t{
-	SOCKET _clientSocket;
-	SOCKADDR_IN _srvAddr;
-
-	Socket_link_t(std::string addr,unsigned int port){
-		WSA.init();
-		_clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-		if (_clientSocket == INVALID_SOCKET)
-			throw (std::runtime_error)"_clientSocket = socket(AF_INET, SOCK_STREAM, 0) execute failed!";
-		//初始化服务器端地址族变量
-		
-		_srvAddr.sin_addr.S_un.S_addr = inet_addr(addr.c_str());
-		_srvAddr.sin_family = AF_INET;
-		_srvAddr.sin_port = htons(port);
-
-		//连接服务器
-		if (connect(_clientSocket, (SOCKADDR*)&_srvAddr, sizeof(SOCKADDR)))
-			throw (std::runtime_error)"connect(_clientSocket, (SOCKADDR*)&_srvAddr, sizeof(SOCKADDR)) execute failed!";
-	}
-	void relink(){
-		closesocket(_clientSocket);
-		_clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-		if (_clientSocket == INVALID_SOCKET)
-			throw (std::runtime_error)"_clientSocket = socket(AF_INET, SOCK_STREAM, 0) execute failed!";
-		if (connect(_clientSocket, (SOCKADDR*)&_srvAddr, sizeof(SOCKADDR)))
-			throw (std::runtime_error)"connect(_clientSocket, (SOCKADDR*)&_srvAddr, sizeof(SOCKADDR)) execute failed!";
-	}
-	Socket_link_t(SOCKET clientSocket){
-		_clientSocket = clientSocket;
-		_srvAddr={};
-	}
-	~Socket_link_t(){
-		//清理
-		closesocket(_clientSocket);
-	}
-	void base_send(std::string massage){
-		//发送消息
-		::send(_clientSocket, massage.c_str(),massage.size(),0);
-	}
-	std::string base_get_ret(){
-		//接收消息
-		std::string massage;
-		char recvBuf[513];
-		while(1){
-			int a= ::recv(_clientSocket, recvBuf, 512, 0);
-			if (a == EAGAIN || a == 0)
-				break;
-			else{
-				recvBuf[512]=0;
-				massage += recvBuf;
-			}
+namespace Socket_link_n{
+	inline struct WSA_t{
+		static inline bool inited = 0;
+		void init(){
+			if(inited)
+				return;
+			WSADATA wsaData = {0};
+			if (WSAStartup(MAKEWORD(2, 1), &wsaData) != 0)
+				throw (std::runtime_error)std::to_string(WSAGetLastError());
 		}
-		return massage;
-	}
-};
+		void uninit(){
+			if(inited)
+				WSACleanup();
+			inited = 0;
+		}
+		~WSA_t(){uninit();}
+	}WSA{};
 
-struct Socket_t{
-	SOCKET _serverSocket;
-	SOCKADDR_IN _srvAddr;
-	Socket_t(unsigned int port){
-		WSA.init();
-		//创建套接字
-		_serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-		if (_serverSocket == INVALID_SOCKET)
-			throw (std::runtime_error)"_serverSocket = socket(AF_INET, SOCK_STREAM, 0) execute failed!";
-		//初始化服务器地址族变量
-		_srvAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
-		_srvAddr.sin_family = AF_INET;
-		_srvAddr.sin_port = htons(port);
+	struct Socket_link_t{
+		SOCKET _clientSocket;
+		SOCKADDR_IN _srvAddr;
 
-		//绑定
-		if (bind(_serverSocket, (SOCKADDR*)&_srvAddr, sizeof(SOCKADDR)) == SOCKET_ERROR)
-			throw (std::runtime_error)"bind(_serverSocket, (SOCKADDR*)&_srvAddr, sizeof(SOCKADDR)) execute failed!";
-	}
-	~Socket_t(){
-		//清理
-		closesocket(_serverSocket);
-	}
-	Socket_link_t get_link(){
-		//等待连接
-		SOCKADDR_IN clientAddr;
-		int len = sizeof(SOCKADDR);
-		SOCKET connSocket = accept(_serverSocket, (SOCKADDR*)&clientAddr, &len);
-		if (connSocket == INVALID_SOCKET)
-			throw (std::runtime_error)"accept(_serverSocket, (SOCKADDR*)&clientAddr, &len) execute failed!";
-		return connSocket;
-	}
-	void listen(){
-		//监听
-		if (::listen(_serverSocket, 10) == SOCKET_ERROR)
-			throw (std::runtime_error)"listen(_serverSocket, 10) execute failed!";
-	}
-};
+		Socket_link_t(std::string addr,unsigned int port){
+			WSA.init();
+			_clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+			if (_clientSocket == INVALID_SOCKET)
+				throw (std::runtime_error)"_clientSocket = socket(AF_INET, SOCK_STREAM, 0) execute failed!";
+			//初始化服务器端地址族变量
+			
+			_srvAddr.sin_addr.S_un.S_addr = inet_addr(addr.c_str());
+			_srvAddr.sin_family = AF_INET;
+			_srvAddr.sin_port = htons(port);
 
+			//连接服务器
+			if (connect(_clientSocket, (SOCKADDR*)&_srvAddr, sizeof(SOCKADDR)))
+				throw (std::runtime_error)"connect(_clientSocket, (SOCKADDR*)&_srvAddr, sizeof(SOCKADDR)) execute failed!";
+		}
+		void relink(){
+			closesocket(_clientSocket);
+			_clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+			if (_clientSocket == INVALID_SOCKET)
+				throw (std::runtime_error)"_clientSocket = socket(AF_INET, SOCK_STREAM, 0) execute failed!";
+			if (connect(_clientSocket, (SOCKADDR*)&_srvAddr, sizeof(SOCKADDR)))
+				throw (std::runtime_error)"connect(_clientSocket, (SOCKADDR*)&_srvAddr, sizeof(SOCKADDR)) execute failed!";
+		}
+		Socket_link_t(SOCKET clientSocket){
+			_clientSocket = clientSocket;
+			_srvAddr={};
+		}
+		~Socket_link_t(){
+			//清理
+			closesocket(_clientSocket);
+		}
+		void base_send(std::string massage){
+			//发送消息
+			::send(_clientSocket, massage.c_str(),massage.size(),0);
+		}
+		std::string base_get_ret(){
+			//接收消息
+			std::string massage;
+			char recvBuf[513];
+			while(1){
+				int a= ::recv(_clientSocket, recvBuf, 512, 0);
+				if (a == EAGAIN || a == 0)
+					break;
+				else{
+					recvBuf[512]=0;
+					massage += recvBuf;
+				}
+			}
+			return massage;
+		}
+	};
+
+	struct Socket_t{
+		SOCKET _serverSocket;
+		SOCKADDR_IN _srvAddr;
+		Socket_t(unsigned int port){
+			WSA.init();
+			//创建套接字
+			_serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+			if (_serverSocket == INVALID_SOCKET)
+				throw (std::runtime_error)"_serverSocket = socket(AF_INET, SOCK_STREAM, 0) execute failed!";
+			//初始化服务器地址族变量
+			_srvAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+			_srvAddr.sin_family = AF_INET;
+			_srvAddr.sin_port = htons(port);
+
+			//绑定
+			if (bind(_serverSocket, (SOCKADDR*)&_srvAddr, sizeof(SOCKADDR)) == SOCKET_ERROR)
+				throw (std::runtime_error)"bind(_serverSocket, (SOCKADDR*)&_srvAddr, sizeof(SOCKADDR)) execute failed!";
+		}
+		~Socket_t(){
+			//清理
+			closesocket(_serverSocket);
+		}
+		Socket_link_t get_link(){
+			//等待连接
+			SOCKADDR_IN clientAddr;
+			int len = sizeof(SOCKADDR);
+			SOCKET connSocket = accept(_serverSocket, (SOCKADDR*)&clientAddr, &len);
+			if (connSocket == INVALID_SOCKET)
+				throw (std::runtime_error)"accept(_serverSocket, (SOCKADDR*)&clientAddr, &len) execute failed!";
+			return connSocket;
+		}
+		void listen(){
+			//监听
+			if (::listen(_serverSocket, 10) == SOCKET_ERROR)
+				throw (std::runtime_error)"listen(_serverSocket, 10) execute failed!";
+		}
+	};
+}
--- sstp.hpp
+++ sstp.hpp
@@ -2,108 +2,111 @@
 #include "base.hpp"
 #include "codepage.hpp"
 
-struct SSTP_link_arg_parts_t{
-	std::wstring _name;
-	std::wstring _var;
-};
-struct SSTP_link_args_t{
-	std::map<std::wstring,std::wstring>_m;
-	SSTP_link_args_t(std::initializer_list<SSTP_link_arg_parts_t>a){
-		for (auto&x:a) {
-			_m[x._name]=x._var;
-		}
-	}
-	SSTP_link_args_t(std::wstring a){
-		if(a.find(L":") > a.find(L"\r\n"))
-			a.erase(0,a.find(L"\r\n"));
-		while(a!=L""){
-			std::wstring t=a.substr(0,a.find(L"\r\n"));
-			if (t.find(L": ")!=t.npos){
-				_m[t.substr(0,t.find(L": "))]=t.substr(t.find(L": ")+2);
-				a.erase(0,a.find(L"\r\n")+2);
-				while (!a.substr(0,a.find(L"\r\n")).find(L": ")){
-					_m[t.substr(0,t.find(L": "))]+=L"\r\n"+a.substr(0,a.find(L"\r\n"));
-					a.erase(0,a.find(L"\r\n")+2);
-				}
-			}else{
-				a.erase(0,a.find(L"\r\n")+2);
+namespace SSTP_link_n{
+	using namespace CODEPAGE_n;
+	using namespace Socket_link_n;
+	struct SSTP_link_arg_parts_t{
+		std::wstring _name;
+		std::wstring _var;
+	};
+	struct SSTP_link_args_t{
+		std::map<std::wstring,std::wstring>_m;
+		SSTP_link_args_t(std::initializer_list<SSTP_link_arg_parts_t>a){
+			for (auto&x:a) {
+				_m[x._name]=x._var;
 			}
 		}
-	}
+		SSTP_link_args_t(std::wstring a){
+			if(a.find(L":") > a.find(L"\r\n"))
+				a.erase(0,a.find(L"\r\n"));
+			while(a!=L""){
+				std::wstring t=a.substr(0,a.find(L"\r\n"));
+				if (t.find(L": ")!=t.npos){
+					_m[t.substr(0,t.find(L": "))]=t.substr(t.find(L": ")+2);
+					a.erase(0,a.find(L"\r\n")+2);
+					while (!a.substr(0,a.find(L"\r\n")).find(L": ")){
+						_m[t.substr(0,t.find(L": "))]+=L"\r\n"+a.substr(0,a.find(L"\r\n"));
+						a.erase(0,a.find(L"\r\n")+2);
+					}
+				}else{
+					a.erase(0,a.find(L"\r\n")+2);
+				}
+			}
+		}
+		template<class T>
+		SSTP_link_args_t(T&&a):SSTP_link_args_t(std::wstring(a)){}
+		operator std::wstring(){
+			std::wstring aret;
+			for (auto&x:_m) {
+				aret += x.first + L": " + x.second + L"\r\n";
+			}
+			return aret;
+		}
+		auto operator[](std::wstring a){return _m[a];}
+	};
+
 	template<class T>
-	SSTP_link_args_t(T&&a):SSTP_link_args_t(std::wstring(a)){}
-	operator std::wstring(){
-		std::wstring aret;
-		for (auto&x:_m) {
-			aret += x.first + L": " + x.second + L"\r\n";
+	auto operator+(T&&a,SSTP_link_args_t&b) {return a+std::wstring(b);}
+
+	struct SSTP_ret_t{
+		std::wstring _m;
+		SSTP_ret_t(std::wstring a):_m(a){}
+		operator std::wstring(){return _m;}
+		explicit operator SSTP_link_args_t(){return _m;}
+		auto to_str(){return _m;}
+		auto to_map(){return operator SSTP_link_args_t();}
+		auto operator[](std::wstring a){return SSTP_link_args_t(_m)[a];}
+	};
+
+	template<class T>
+	auto&&operator<<(T&&a,SSTP_ret_t b) {return a<<b.to_str();}
+	template<class T>
+	auto operator+(T&&a,SSTP_ret_t b) {return a+b.to_str();}
+
+	struct SSTP_link_t:Socket_link_t{
+		SSTP_link_args_t _header;
+
+		SSTP_link_t(
+					SSTP_link_args_t header={{L"Charset",L"UTF-8"},{L"Sender",L"void"}},
+					std::string addr="127.0.0.1",unsigned int port=9821
+					):
+			Socket_link_t(addr,port),_header(header){}
+
+		std::wstring get_SSTP_head(std::wstring SSTP_type){
+			return SSTP_type+L"\r\n"+_header;
 		}
-		return aret;
-	}
-	auto operator[](std::wstring a){return _m[a];}
-};
-
-template<class T>
-auto operator+(T&&a,SSTP_link_args_t&b) {return a+std::wstring(b);}
-
-struct SSTP_ret_t{
-	std::wstring _m;
-	SSTP_ret_t(std::wstring a):_m(a){}
-	operator std::wstring(){return _m;}
-	explicit operator SSTP_link_args_t(){return _m;}
-	auto to_str(){return _m;}
-	auto to_map(){return operator SSTP_link_args_t();}
-	auto operator[](std::wstring a){return SSTP_link_args_t(_m)[a];}
-};
-
-template<class T>
-auto&&operator<<(T&&a,SSTP_ret_t b) {return a<<b.to_str();}
-template<class T>
-auto operator+(T&&a,SSTP_ret_t b) {return a+b.to_str();}
-
-struct SSTP_link_t:Socket_link_t{
-	SSTP_link_args_t _header;
-
-	SSTP_link_t(
-				SSTP_link_args_t header={{L"Charset",L"UTF-8"},{L"Sender",L"void"}},
-				std::string addr="127.0.0.1",unsigned int port=9821
-				):
-		Socket_link_t(addr,port),_header(header){}
-
-	std::wstring get_SSTP_head(std::wstring SSTP_type){
-		return SSTP_type+L"\r\n"+_header;
-	}
-	std::wstring base_SSTP_send(std::wstring head,SSTP_link_args_t args){
-		relink();
-		{
-			auto send=get_SSTP_head(head)+args+L"\r\n";
-			auto charset_begin=send.find(L"\r\nCharset: ")+11;
-			auto charset=send.substr(charset_begin,send.find(L"\r\n",charset_begin));
-			base_send(UnicodeToMultiByte(send,StringtoCodePage(charset.c_str()),0));
+		std::wstring base_SSTP_send(std::wstring head,SSTP_link_args_t args){
+			relink();
+			{
+				auto send=get_SSTP_head(head)+args+L"\r\n";
+				auto charset_begin=send.find(L"\r\nCharset: ")+11;
+				auto charset=send.substr(charset_begin,send.find(L"\r\n",charset_begin));
+				base_send(UnicodeToMultiByte(send,StringtoCodePage(charset.c_str()),0));
+			}
+			{
+				auto temp = base_get_ret();
+				auto charset_begin=temp.find("\r\nCharset: ")+11;
+				auto charset=temp.substr(charset_begin,temp.find("\r\n",charset_begin));
+				return MultiByteToUnicode(temp,StringtoCodePage(charset.c_str()),0);
+			}
 		}
-		{
-			auto temp = base_get_ret();
-			auto charset_begin=temp.find("\r\nCharset: ")+11;
-			auto charset=temp.substr(charset_begin,temp.find("\r\n",charset_begin));
-			return MultiByteToUnicode(temp,StringtoCodePage(charset.c_str()),0);
+		SSTP_ret_t NOTYFY(SSTP_link_args_t args){
+			return base_SSTP_send(L"NOTIFY SSTP/1.1",args);
 		}
-	}
-	SSTP_ret_t NOTYFY(SSTP_link_args_t args){
-		return base_SSTP_send(L"NOTIFY SSTP/1.1",args);
-	}
-	SSTP_ret_t SEND(SSTP_link_args_t args){
-		return base_SSTP_send(L"SEND SSTP/1.4",args);
-	}
-	SSTP_ret_t EXECUTE(SSTP_link_args_t args){
-		return base_SSTP_send(L"EXECUTE SSTP/1.3",args);
-	}
-	SSTP_ret_t COMMUNICATE(SSTP_link_args_t args){
-		return base_SSTP_send(L"COMMUNICATE SSTP/1.1",args);
-	}
-	SSTP_ret_t GIVE(SSTP_link_args_t args){
-		return base_SSTP_send(L"GIVE SSTP/1.1",args);
-	}
-	SSTP_ret_t INSTALL(SSTP_link_args_t args){
-		return base_SSTP_send(L"INSTALL SSTP/1.0",args);
-	}
-};
-
+		SSTP_ret_t SEND(SSTP_link_args_t args){
+			return base_SSTP_send(L"SEND SSTP/1.4",args);
+		}
+		SSTP_ret_t EXECUTE(SSTP_link_args_t args){
+			return base_SSTP_send(L"EXECUTE SSTP/1.3",args);
+		}
+		SSTP_ret_t COMMUNICATE(SSTP_link_args_t args){
+			return base_SSTP_send(L"COMMUNICATE SSTP/1.1",args);
+		}
+		SSTP_ret_t GIVE(SSTP_link_args_t args){
+			return base_SSTP_send(L"GIVE SSTP/1.1",args);
+		}
+		SSTP_ret_t INSTALL(SSTP_link_args_t args){
+			return base_SSTP_send(L"INSTALL SSTP/1.0",args);
+		}
+	};
+}
